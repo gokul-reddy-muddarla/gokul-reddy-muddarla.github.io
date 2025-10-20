f6364fc566656a4e9747c1f55a9f873224ac4b91
Gokul Reddy <gokul@Gokuls-MacBook-Pro.local>
2025-10-19 21:21:04 -0700

    Add out

---
+++ out/_next/static/chunks/e4377b6301ba53ec.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/48cc3388ebede352.js",
+  "static/chunks/turbopack-f76a9a661a428af6.js"
+])
---
+++ out/_next/static/chunks/f524cdc4935fb6f2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/48cc3388ebede352.js",
+  "static/chunks/turbopack-5f809b81a2fda681.js"
+])
